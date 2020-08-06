eeb0c1531946392cd76ea2c17fb7ebccb4ebed74
XiWJ <fdyz_xwj@163.com>
2020-08-06 19:37:56 +0800

    Update ComputeNormal.py

--- ComputeNormal/pytorch-code/ComputeNormal.py
+++ ComputeNormal/pytorch-code/ComputeNormal.py
@@ -26,6 +26,7 @@ if __name__ == '__main__':
 
     # load depth & intrinsic
     depth_np = np.load(depth_path)
+    H, W = depth_np.shape
     depth_torch = torch.from_numpy(depth_np).unsqueeze(0).unsqueeze(-1) # (B, h, w, 1)
     valid_depth = depth_np > 0.0
     intrinsic_np = np.load(intrinsic_path)
@@ -38,10 +39,10 @@ if __name__ == '__main__':
     point_matrix = F.unfold(points, kernel_size=5, stride=1, padding=4, dilation=2)
 
     # An = b
-    matrix_a = point_matrix.view(1, 3, 25, 192, 256)  # (B, 3, 25, HxW)
+    matrix_a = point_matrix.view(1, 3, 25, H, W)  # (B, 3, 25, HxW)
     matrix_a = matrix_a.permute(0, 3, 4, 2, 1) # (B, HxW, 25, 3)
     matrix_a_trans = matrix_a.transpose(3, 4)
-    matrix_b = torch.ones([1, 192, 256, 25, 1])
+    matrix_b = torch.ones([1, H, W, 25, 1])
 
     # dot(A.T, A)
     point_multi = torch.matmul(matrix_a_trans, matrix_a)
@@ -54,7 +55,7 @@ if __name__ == '__main__':
     diag_constant = torch.ones([3], dtype=torch.float32)
     diag_element = torch.diag(diag_constant)
     diag_element = diag_element.unsqueeze(0).unsqueeze(0).unsqueeze(0)
-    diag_matrix = diag_element.repeat(1, 192, 256, 1, 1)
+    diag_matrix = diag_element.repeat(1, H, W, 1, 1)
     # inversible matrix
     inversible_matrix = torch.where(inverse_condition_all, point_multi, diag_matrix)
     inv_matrix = torch.inverse(inversible_matrix.to("cpu"))
