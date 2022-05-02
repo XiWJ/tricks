27e38b0e73d924c060100dd6196d5e85bad23dd9
XiWeijie <fdyz_xwj@163.com>
2022-05-02 17:07:38 +0800

    Update (shader.h)

--- LearnOpenGL/include/tools/shader_s.h
+++ LearnOpenGL/include/tools/shader_s.h
@@ -101,15 +101,27 @@ public:
     {
         glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y); 
     }
+    void setVec2(const std::string &name, const glm::vec2 &value) const
+    {
+        glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+    }
     // ------------------------------------------------------------------------
     void setVec3(const std::string &name, float x, float y, float z) const
     {
         glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z); 
     }
+    void setVec3(const std::string &name, const glm::vec3 &value) const
+    {
+        glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+    }
     // ------------------------------------------------------------------------
     void setVec4(const std::string &name, float x, float y, float z, float w) const
     {
         glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w); 
+    }
+    void setVec4(const std::string &name, const glm::vec4 &value) const
+    {
+        glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
     }
     // ------------------------------------------------------------------------
     void setMat2(const std::string &name, const glm::mat2 &mat) const
